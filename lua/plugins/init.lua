554281f6fed525d46ec58c289bcfac5cb752cb68
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2025-02-05 14:18:14 +0800

    a good starting point

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,29 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local opts = require "nvchad.configs.cmp"
+      -- I prefer not to select the first one (noselect)
+      opts.completion.completeopt = "menu,menuone,noselect"
+      return opts
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "nix",
+        "json",
+        "toml",
+      },
+    },
+  },
 }
