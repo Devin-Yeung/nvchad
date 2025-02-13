3e04d37fd400c03983f8b455b75fcba85f1bf244
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2025-02-13 20:44:45 +0800

    feat: reconfig ocamllsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,13 +4,13 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "lua_ls", "clangd", "rust_analyzer", "pyright", "ocamllsp" }
+local servers = { "html", "cssls", "lua_ls", "clangd", "rust_analyzer", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = function (client, bufnr)
+    on_attach = function(client, bufnr)
       -- https://github.com/mrcjkb/rustaceanvim/discussions/46#discussioncomment-7636177
       vim.lsp.inlay_hint.enable(true)
       nvlsp.on_attach(client, bufnr)
@@ -19,6 +19,25 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+lspconfig.ocamllsp.setup {
+  on_attach = function(client, bufnr)
+    -- https://github.com/mrcjkb/rustaceanvim/discussions/46#discussioncomment-7636177
+    vim.lsp.inlay_hint.enable(true)
+    nvlsp.on_attach(client, bufnr)
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  -- See: https://github.com/ocaml/ocaml-lsp/blob/master/ocaml-lsp-server/docs/ocamllsp/config.md
+  settings = {
+    inlayHints = {
+      enable = true,
+    },
+    codelens = {
+      enable = true,
+    },
+  },
+}
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
