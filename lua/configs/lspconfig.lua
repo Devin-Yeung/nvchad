f4b79a3780ec6ede024a7833cc088704d4930e86
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2025-02-13 21:03:36 +0800

    refactor: refactor on_attach

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,26 +6,30 @@ local lspconfig = require "lspconfig"
 -- EXAMPLE
 local servers = { "html", "cssls", "lua_ls", "clangd", "rust_analyzer", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
+local on_attach = function(client, bufnr)
+  nvlsp.on_attach(client, bufnr)
+  -- https://github.com/mrcjkb/rustaceanvim/discussions/46#discussioncomment-7636177
+  -- https://gist.github.com/Chattille/adbd1f296b03bc3f85bb7f8d6f648c6f
+  vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+    buffer = bufnr,
+    callback = vim.lsp.codelens.refresh,
+  })
+  -- manually call a refresh
+  vim.lsp.inlay_hint.enable(true)
+  vim.lsp.codelens.refresh()
+end
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = function(client, bufnr)
-      -- https://github.com/mrcjkb/rustaceanvim/discussions/46#discussioncomment-7636177
-      vim.lsp.inlay_hint.enable(true)
-      nvlsp.on_attach(client, bufnr)
-    end,
+    on_attach = on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
 end
 
 lspconfig.ocamllsp.setup {
-  on_attach = function(client, bufnr)
-    -- https://github.com/mrcjkb/rustaceanvim/discussions/46#discussioncomment-7636177
-    vim.lsp.inlay_hint.enable(true)
-    nvlsp.on_attach(client, bufnr)
-  end,
+  on_attach = on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
   -- See: https://github.com/ocaml/ocaml-lsp/blob/master/ocaml-lsp-server/docs/ocamllsp/config.md
